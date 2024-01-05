3862645d01288db63ddb46f70b177496e72db3e8
Ismail099 <hedaribo09@gmail.com>
2024-01-05 21:16:59 +0100

    Fixes

--- js/functions.js
+++ js/functions.js
@@ -4,35 +4,35 @@ var clientWidth = $(window).width();
 var clientHeight = $(window).height();
 
 $(function () {
-    // setup garden
+	// setup garden
 	$loveHeart = $("#loveHeart");
 	var offsetX = $loveHeart.width() / 2;
 	var offsetY = $loveHeart.height() / 2 - 55;
-    $garden = $("#garden");
-    gardenCanvas = $garden[0];
+	$garden = $("#garden");
+	gardenCanvas = $garden[0];
 	gardenCanvas.width = $("#loveHeart").width();
-    gardenCanvas.height = $("#loveHeart").height()
-    gardenCtx = gardenCanvas.getContext("2d");
-    gardenCtx.globalCompositeOperation = "lighter";
-    garden = new Garden(gardenCtx, gardenCanvas);
-	
+	gardenCanvas.height = $("#loveHeart").height()
+	gardenCtx = gardenCanvas.getContext("2d");
+	gardenCtx.globalCompositeOperation = "lighter";
+	garden = new Garden(gardenCtx, gardenCanvas);
+
 	$("#content").css("width", $loveHeart.width() + $("#code").width());
 	$("#content").css("height", Math.max($loveHeart.height(), $("#code").height()));
 	$("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2, 10));
 	$("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
 
-    // renderLoop
-    setInterval(function () {
-        garden.render();
-    }, Garden.options.growSpeed);
+	// renderLoop
+	setInterval(function () {
+		garden.render();
+	}, Garden.options.growSpeed);
 });
 
-$(window).resize(function() {
-    var newWidth = $(window).width();
-    var newHeight = $(window).height();
-    if (newWidth != clientWidth && newHeight != clientHeight) {
-        location.replace(location);
-    }
+$(window).resize(function () {
+	var newWidth = $(window).width();
+	var newHeight = $(window).height();
+	if (newWidth != clientWidth && newHeight != clientHeight) {
+		location.replace(location);
+	}
 });
 
 function getHeartPoint(angle) {
@@ -70,12 +70,12 @@ function startHeartAnimation() {
 	}, interval);
 }
 
-(function($) {
-	$.fn.typewriter = function() {
-		this.each(function() {
+(function ($) {
+	$.fn.typewriter = function () {
+		this.each(function () {
 			var $ele = $(this), str = $ele.html(), progress = 0;
 			$ele.html('');
-			var timer = setInterval(function() {
+			var timer = setInterval(function () {
 				var current = str.substr(progress, 1);
 				if (current == '<') {
 					progress = str.indexOf('>', progress) + 1;
@@ -92,31 +92,41 @@ function startHeartAnimation() {
 	};
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
+function timeElapse(date) {
+	var current = new Date();
+	var timeDifference = current - date;
+
+
+	//Arrange the difference of date in days, hours, minutes, and seconds format
+	let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+	let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+	let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	seconds = seconds % 60;
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
+
+	var result = `<span class="digit">${days}</span> days <span class="digit">${hours}</span> hours <span class="digit">${minutes}</span> minutes <span class="digit">${seconds}</span> seconds`;
 	$("#elapseClock").html(result);
 }
 
+// Example usage:
+// var together = new Date(2022, 11, 5, 12, 33, 0, 0); // Set your target date
+// setInterval(function() {
+//   timeElapse(together);
+// }, 1000);
+
+
 function showMessages() {
 	adjustWordsPosition();
-	$('#messages').fadeIn(5000, function() {
+	$('#messages').fadeIn(5000, function () {
 		showLoveU();
 	});
 }
