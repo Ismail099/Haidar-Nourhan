f24e82981457c83cb0f796634541401cee398ca2
Ismail099 <hedaribo09@gmail.com>
2024-11-02 21:01:28 +0100

    hopefully the last one

--- js/functions.js
+++ js/functions.js
@@ -103,6 +103,7 @@ function timeElapse(date) {
 	let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 	let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
+	/*
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
@@ -112,7 +113,7 @@ function timeElapse(date) {
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-
+	*/
 	var result = `<span class="digit">${days}</span> days <span class="digit">${hours}</span> hours <span class="digit">${minutes}</span> minutes <span class="digit">${seconds}</span> seconds`;
 	$("#elapseClock").html(result);
 }
